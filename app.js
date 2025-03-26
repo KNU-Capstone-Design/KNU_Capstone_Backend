06ff5ca36c20f4f7b75ce52288557c26ecc258b0
kimsunho2000 <sunhokim28@gmail.com>
2025-03-26 16:41:53 +0900

    mongoose 설치 및 type:module추가,Mongodb연결 완료: :

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 /* Entry Point */
-//tmp
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
+import connectDB from './src/config/mongoose.js'
+import express from 'express'
 
 const app = express();
 const port = 3000;
 
-// Swagger 문서 연결
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// 기본 라우트
-/**
- * @swagger
- * /:
- *   get:
- *     summary: 기본 경로 응답
- *     description: 루트 경로('/')의 응답을 반환합니다.
- *     responses:
- *       200:
- *         description: 성공적으로 응답 반환
- */
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// DB실행
+connectDB();
 
 // 서버 실행
 app.listen(port, () => {
