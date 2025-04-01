51064867ef5d281a7efea5e101e51b267c8436a5
kimsunho2000 <sunhokim28@gmail.com>
2025-04-01 15:19:05 +0900

    Restful하게 /api/answers 항목들 수정, Swagger에 반영

--- src/models/users.js
+++ src/models/users.js
@@ -4,9 +4,13 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     // 사용자의 이메일
     email: { type: String, required: true, unique: true },
+    // 사용자가 구독한 카테고리
+    categories: [{ type: String}],
     // 사용자의 구독 상태 (기본값은 false, 구독 중이면 true)
     subscriptionStatus: { type: Boolean, default: false }
+
 });
 
 const User = mongoose.model('User', userSchema);
+
 export default User;
