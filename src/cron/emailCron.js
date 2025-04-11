2a3a02c47500e7785a2e9f6349dc14bdb54502fb
kimsunho2000 <sunhokim28@gmail.com>
2025-04-11 21:42:30 +0900

    이메일 발송시간 서울기준 9시로 설정, npm install 필요

--- src/cron/emailCron.js
+++ src/cron/emailCron.js
@@ -1,13 +1,14 @@
 // 서버에서 정해진 시간마다 스케줄링해서 이메일 전송 시키는 비지니스 로직
 
 import cron from 'node-cron'
+import moment from 'moment-timezone'
 import { getSubscribedUsers } from '../services/subscribeService.js';
 import { sendQuestionEmail } from '../services/mailService.js';
 
 // 매일 9시마다 실행
-cron.schedule('0 9 * * *', async () => {
-    console.log('[매일 아침 9시 이메일 전송 시작');
-
+cron.schedule('0 0 9 * * *', async () => {
+    const koreaTime = moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
+    console.log(`${koreaTime}:매일 아침 9시 이메일 전송 시작`);
     const users = await getSubscribedUsers(); // 이메일 보낼 대상
     for (const user of users) {
         await sendQuestionEmail({
@@ -15,6 +16,7 @@ cron.schedule('0 9 * * *', async () => {
             questionId: '랜덤 또는 오늘의 질문 ID',
         });
     }
-
     console.log('[CRON] 이메일 전송 완료');
+    }, {
+    timezone: "Asia/Seoul"
 });
