82a120ffaf7f672ecf7ff6bd70909a2322309f39
kimsunho2000 <sunhokim28@gmail.com>
2025-08-03 02:37:47 +0900

    SMTP,API 어드민 페이지 디버깅

--- src/cron/recordCron.js
+++ src/cron/recordCron.js
@@ -1,11 +1,12 @@
 import cron from 'node-cron';
 import { SmtpUsage } from "../models/smtpUsage.js";
 import { getKSTDateString } from "../utils/date.js";
+import logger from '../utils/logger.js';
 
 // 매일 자정(0시)에 SMTP 사용량 문서를 생성
 cron.schedule('0 0 0 * * *', async () => {
+    const todayDate = getKSTDateString();
     try {
-        const todayDate = getKSTDateString();
         // 해당 날짜의 기록이 없으면 생성
         const exists = await SmtpUsage.findOne({ date: todayDate });
 
@@ -18,7 +19,10 @@ cron.schedule('0 0 0 * * *', async () => {
         }
     }
     catch (error) {
-        console.error('[CRON] SMTP 사용량 기록 중 오류 발생:', error);
+        logger.error('SMTP 사용량 기록 크론 작업 중 오류 발생', {
+            date: todayDate,
+            error: error.stack || error.message
+        });
     }
 }, {
     timezone: "Asia/Seoul"
